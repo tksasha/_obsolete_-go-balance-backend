9645a8a400925ca2414ef51d5ec2ea4df79b3ae0
Alexander Tkachuk <mail@tksasha.me>
2016-01-21 17:43:40 +0200

    use Errors from `github.com/tksasha/go-errors`

--- creators.go
+++ creators.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "net/url"
+
+  . "github.com/tksasha/go-errors"
 )
 
 //
--- validators.go
+++ validators.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+  . "github.com/tksasha/go-errors"
+)
+
 //
 // Category
 //
