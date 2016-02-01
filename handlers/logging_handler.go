3eb96f368f364c78aad062538e04bc4f17e9a419
Alexander Tkachuk <mail@tksasha.me>
2016-02-01 11:12:05 +0200

    use `LoggingHandler` from `gorilla/handlers`

--- handlers/logging_handler.go
+++ handlers/logging_handler.go
@@ -2,13 +2,11 @@ package handlers
 
 import (
   "net/http"
-  "log"
+  "os"
+
+  "github.com/gorilla/handlers"
 )
 
 func LoggingHandler(h http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    log.Printf("%s %s", r.Method, r.URL)
-
-    h.ServeHTTP(w, r)
-  })
+  return handlers.LoggingHandler(os.Stdout, h)
 }
--- router/router.go
+++ router/router.go
@@ -12,14 +12,25 @@ import (
 func NewRouter() http.Handler {
   router := httprouter.New()
 
+  //
+  // /categories
+  //
   router.GET("/categories", new(Categories).Index)
 
   router.POST("/categories", new(Categories).Create)
 
+  router.PATCH("/categories/:id", new(Categories).Update)
+
+  //
+  // /items
+  //
   router.GET("/items", new(Items).Index)
 
   router.POST("/items", new(Items).Create)
 
+  //
+  // /consolidates
+  //
   router.GET("/consolidates", new(Consolidates).Index)
 
   return alice.New(LoggingHandler, ContentTypeHandler).Then(router)
