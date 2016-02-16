ecf8486bf303e3ae735fb1307380e674d53aeb87
Alexander Tkachuk <mail@tksasha.me>
2016-02-16 13:53:43 +0200

    fix test (it used test database)

--- config/test/initializer.go
+++ config/test/initializer.go
@@ -2,20 +2,10 @@ package test
 
 import (
 	"net/url"
-
-	"github.com/tksasha/balance/Godeps/_workspace/src/github.com/jinzhu/gorm"
-	_ "github.com/tksasha/balance/Godeps/_workspace/src/github.com/mattn/go-sqlite3"
-
-	. "github.com/tksasha/balance/config"
-	. "github.com/tksasha/balance/models"
 )
 
 var Values url.Values
 
 func init() {
-	DB, _ = gorm.Open("sqlite3", DBWorkDir+"test.sqlite3")
-
-	DB.AutoMigrate(&Item{}, &Category{})
-
 	Values = make(url.Values)
 }
