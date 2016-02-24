13d00422a86c347da7bece7b3ad16b53d915749b
Alexander Tkachuk <mail@tksasha.me>
2016-02-24 13:08:07 +0200

    tiny improvement for ValidateGreaterThan

--- rest/validations/validate_greater_than.go
+++ rest/validations/validate_greater_than.go
@@ -14,48 +14,16 @@ func ValidateGreaterThan(resource Resource, attribute string, value interface{})
 
   switch value.(type) {
   case float64:
-    value := toFloat64(value)
-
-    if v.Interface().(float64) <= value {
+    if v.Interface().(float64) <= value.(float64) {
       message := fmt.Sprintf("can't be less than or equal `%f`", value)
 
       resource.Errors().Add(attribute, message)
     }
   case int:
-    value := toInt(value)
-
-    if v.Interface().(int) <= value {
+    if v.Interface().(int) <= value.(int) {
       message := fmt.Sprintf("can't be less than or equal `%d`", value)
 
       resource.Errors().Add(attribute, message)
     }
-  }
-}
-
-func toFloat64(i interface{}) float64 {
-  value := reflect.ValueOf(i)
-
-  value = reflect.Indirect(value)
-
-  floatType := reflect.TypeOf(float64(0))
-
-  if value.Type().ConvertibleTo(floatType) {
-    return value.Convert(floatType).Float()
-  } else {
-    return 0.0
-  }
-}
-
-func toInt(i interface{}) int {
-  value := reflect.ValueOf(i)
-
-  value = reflect.Indirect(value)
-
-  intType := reflect.TypeOf(int(0))
-
-  if value.Type().ConvertibleTo(intType) {
-    return int(value.Convert(intType).Int())
-  } else {
-    return 0
   }
 }
