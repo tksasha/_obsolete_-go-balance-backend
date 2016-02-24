749de78ebc556a6366d89c8508e9ab666839203b
Alexander Tkachuk <mail@tksasha.me>
2016-02-24 12:43:43 +0200

    add tests for ValidatePresenceOf

---
+++ rest/validations/validate_presence_of_test.go
@@ -0,0 +1,34 @@
+package validations_test
+
+import (
+  "testing"
+  "time"
+
+  "github.com/stretchr/testify/assert"
+
+  . "github.com/tksasha/balance/rest/validations"
+  . "github.com/tksasha/balance/rest/model"
+)
+
+type M struct {
+  Model
+
+  Name  string
+  Date  time.Time
+}
+
+func TestValidatePresenceOfString(t *testing.T) {
+  m := new(M)
+
+  ValidatePresenceOf(m, "Name")
+
+  assert.Equal(t, []string{"can't be blank"}, m.Errors().Get("Name"))
+}
+
+func TestValidatePresenceOfTime(t *testing.T) {
+  m := new(M)
+
+  ValidatePresenceOf(m, "Date")
+
+  assert.Equal(t, []string{"can't be blank"}, m.Errors().Get("Date"))
+}
